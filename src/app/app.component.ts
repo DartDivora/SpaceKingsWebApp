17744f7ee88eef0babb50448e6778320729a2851
DartDivora <nsanft@sanft.com>
2018-04-04 18:50:11 -0500

    Removed bootstrap library
    1) Removed the bootstrap library from the src/lib directory.
    2) Updated the index.html to use bootstrap from the bootstrap cdn.
    3) Removed call styling in app.component.css.
    4) Made app.component.ts prettier.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,20 @@ import { Component } from '@angular/core';
     selector: 'my-app',
     template: `
     <div>
-        <nav class='navbar navbar-default' role = "navigation" >
-            <div class='container-fluid'>
-                <a class='navbar-brand' href="#">Space Kings Web App</a>
-                <ul class='nav navbar-nav '>
-                    <li><a routerLink="/card">Card</a></li>
-                    <li><a routerLink="/welcome">Welcome</a></li>
-                </ul>
-            </div>
-        </nav>
-        <div class='container'>
-            <router-outlet></router-outlet>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark" role = "navigation" >
+        <div class='container-fluid'>
+            <a class='navbar-brand' href="#">Space Kings Web App</a>
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item"><a class="nav-link" routerLink="/welcome">Welcome</a></li>
+                <li class="nav-item"><a class="nav-link" routerLink="/card">Card</a></li>
+            </ul>
         </div>
-     </div>
+    </nav>
+    <br><br><br>
+    <div class='container'>
+        <router-outlet></router-outlet>
+    </div>
+ </div>
     `
 })
 export class AppComponent {}
